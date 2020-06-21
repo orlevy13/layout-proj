840ebecd570973548f6299b663c80d43a2e7846f
orlevy13 <orlevy13d@gmail.com>
2020-06-21 15:51:06 +0300

    Updates

---
+++ precache-manifest.1e13e2d9d5c9f79182a50752c44733e9.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551d8dbfd52bca4ad9ad2d955ee217c1",
+    "url": "/layout-proj/index.html"
+  },
+  {
+    "revision": "45e6621fa05a16a5f42b",
+    "url": "/layout-proj/static/css/main.027d9b66.chunk.css"
+  },
+  {
+    "revision": "8cc7663fd1ba128ccba6",
+    "url": "/layout-proj/static/js/2.0e88fa8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/layout-proj/static/js/2.0e88fa8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45e6621fa05a16a5f42b",
+    "url": "/layout-proj/static/js/main.bfa9f60f.chunk.js"
+  },
+  {
+    "revision": "1278e2dc9a6f4a1c5569",
+    "url": "/layout-proj/static/js/runtime-main.824040e2.js"
+  },
+  {
+    "revision": "1727d7d1fb2882b66168996105d82a38",
+    "url": "/layout-proj/static/media/Raleway-Bold.1727d7d1.ttf"
+  },
+  {
+    "revision": "ab90c33bd623a29195d2cb3a3cbae6f5",
+    "url": "/layout-proj/static/media/Raleway-Regular.ab90c33b.ttf"
+  },
+  {
+    "revision": "dc7bceaa5cca20023e332a65fc9aa2a1",
+    "url": "/layout-proj/static/media/berkshireswash-regular.dc7bceaa.ttf"
+  },
+  {
+    "revision": "376a8b95194fb33e0a203bf52f8cb467",
+    "url": "/layout-proj/static/media/bg-curvy-desktop.376a8b95.svg"
+  },
+  {
+    "revision": "5f4b8b3e0d56ac7578b1468584142faf",
+    "url": "/layout-proj/static/media/icon-access-anywhere.5f4b8b3e.svg"
+  },
+  {
+    "revision": "aedb3ae079501c43082669d8d427bf6b",
+    "url": "/layout-proj/static/media/icon-any-file.aedb3ae0.svg"
+  },
+  {
+    "revision": "794c5a484aefc6409c3a4e7aceb10eee",
+    "url": "/layout-proj/static/media/icon-arrow.794c5a48.svg"
+  },
+  {
+    "revision": "ca8c71cfbd876e461e850fcdcf9c9b29",
+    "url": "/layout-proj/static/media/icon-collaboration.ca8c71cf.svg"
+  },
+  {
+    "revision": "c90d96c2492a2d6f0863256f2bf9015d",
+    "url": "/layout-proj/static/media/icon-email.c90d96c2.svg"
+  },
+  {
+    "revision": "0f65814bd9f282828254e3382e223a57",
+    "url": "/layout-proj/static/media/icon-location.0f65814b.svg"
+  },
+  {
+    "revision": "47e54a44b879380ff8adb31f40249296",
+    "url": "/layout-proj/static/media/icon-phone.47e54a44.svg"
+  },
+  {
+    "revision": "f558537850876022cbf1d99e525724f4",
+    "url": "/layout-proj/static/media/icon-security.f5585378.svg"
+  },
+  {
+    "revision": "526544ae64c3d7a9cf06bc566728d2b9",
+    "url": "/layout-proj/static/media/illustration-intro.526544ae.png"
+  },
+  {
+    "revision": "99294b1f2757d664921ec643a982959a",
+    "url": "/layout-proj/static/media/illustration-stay-productive.99294b1f.png"
+  },
+  {
+    "revision": "c71bf7bf6985f3ac03fa893fb45bed38",
+    "url": "/layout-proj/static/media/logo.c71bf7bf.svg"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/layout-proj/static/media/open-sans-bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/layout-proj/static/media/open-sans.3ed9575d.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/layout-proj/static/media/roboto-black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/layout-proj/static/media/roboto-bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/layout-proj/static/media/roboto-light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/layout-proj/static/media/roboto-regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/layout-proj/static/media/roboto-thin.321de678.ttf"
+  },
+  {
+    "revision": "65099f98c7cb19b3dac57b15a6a708cf",
+    "url": "/layout-proj/static/media/segoe-ui-bold.65099f98.ttf"
+  },
+  {
+    "revision": "0e7e9a9b5c4abaadef7bc8f4e4574084",
+    "url": "/layout-proj/static/media/segoe-ui.0e7e9a9b.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/layout-proj/static/media/ubuntu.2505bfbd.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16d17ba608cdd09995795235a5c1ced1.js"
+  "/layout-proj/precache-manifest.1e13e2d9d5c9f79182a50752c44733e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/layout-proj/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
